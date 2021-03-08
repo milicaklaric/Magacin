dedbb8df6cb383b4e680969a8534e629c962c448
milicaklaric <milklaric15@gmail.com>
2021-03-08 15:50:24 +0100

    Napravljen projekat Magacin i u njemu klasa Artikal sa svojim atributima

---
+++ Magacin/src/artikal/Artikal.java
@@ -0,0 +1,57 @@
+package artikal;
+
+public class Artikal {
+
+	private int sifra;
+	private String naziv;
+	private String opis;
+	private int kolicina;
+	
+	public Artikal(int sifra, String naziv, String opis, int kolicina) {
+		super();
+		this.sifra = sifra;
+		this.naziv = naziv;
+		this.opis = opis;
+		this.kolicina = kolicina;
+	}
+
+	public int getSifra() {
+		return sifra;
+	}
+
+	public void setSifra(int sifra) {
+		this.sifra = sifra;
+	}
+
+	public String getNaziv() {
+		return naziv;
+	}
+
+	public void setNaziv(String naziv) {
+		this.naziv = naziv;
+	}
+
+	public String getOpis() {
+		return opis;
+	}
+
+	public void setOpis(String opis) {
+		this.opis = opis;
+	}
+
+	public int getKolicina() {
+		return kolicina;
+	}
+
+	public void setKolicina(int kolicina) {
+		this.kolicina = kolicina;
+	}
+
+	@Override
+	public String toString() {
+		return "Artikal [sifra=" + sifra + ", naziv=" + naziv + ", opis=" + opis + ", kolicina=" + kolicina + "]";
+	}
+	
+	
+	
+}
