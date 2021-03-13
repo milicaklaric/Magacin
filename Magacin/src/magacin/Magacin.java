594d845aaf030dbfec58af73d469cee84e53713f
milicaklaric <milklaric15@gmail.com>
2021-03-14 00:58:09 +0100

    Dodata funkcionalnost metode dodajArtikal()

--- Magacin/src/magacin/Magacin.java
+++ Magacin/src/magacin/Magacin.java
@@ -1,13 +1,24 @@
 package magacin;
 
+import java.util.LinkedList;
+
 import interfejs.MagacinInterface;
 
 public class Magacin implements MagacinInterface {
+	LinkedList<Artikal> artikli = new LinkedList<Artikal>();
 
 	@Override
 	public void dodajArtikal(Artikal artikal) {
-		// TODO Auto-generated method stub
-
+		boolean postoji = false;
+		for (Artikal a : artikli) {
+			if(a.equals(artikal)) {
+				postoji = true;
+				a.setKolicina(a.getKolicina()+artikal.getKolicina());
+			}
+		}
+		
+		if (!postoji)
+			artikli.add(artikal);
 	}
 
 	@Override
