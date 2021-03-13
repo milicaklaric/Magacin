7f5141916790c8ff8d11fdd19410327eb5f0e092
milicaklaric <milklaric15@gmail.com>
2021-03-14 00:52:56 +0100

    Dodate funkcionalnosti u klasi Magacin

--- Magacin/src/magacin/Magacin.java
+++ Magacin/src/magacin/Magacin.java
@@ -1,25 +1,44 @@
 package magacin;
 
+import java.util.LinkedList;
+
 import interfejs.MagacinInterface;
 
 public class Magacin implements MagacinInterface {
+	LinkedList<Artikal> artikli = new LinkedList<Artikal>();
 
 	@Override
 	public void dodajArtikal(Artikal artikal) {
-		// TODO Auto-generated method stub
+		if (artikli.contains(artikal)) {
+			for (Artikal a : artikli) {
+				if(a.equals(artikal)) {
+					a.setKolicina(a.getKolicina()+artikal.getKolicina());
+				}
+			}
+		}else {
+			artikli.add(artikal);
+		}
 
 	}
 
 	@Override
 	public void izbaciArtikal(Artikal artikal) {
-		// TODO Auto-generated method stub
-
+		for (Artikal a : artikli) {
+			if (a.equals(artikal)) {
+				a.setKolicina(a.getKolicina() - artikal.getKolicina());
+			}
+		}
 	}
 
 	@Override
 	public Artikal pronadjiArtikal(int sifra) {
-		// TODO Auto-generated method stub
-		return null;
+		Artikal a = null;
+		for (Artikal artikal : artikli) {
+			if (artikal.getSifra()==sifra) {
+				a = artikal;
+			}
+		}
+		return a;
 	}
 
 }
